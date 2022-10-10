fdb23d115282cc8ece17a41980fd8b699bf386d4
SatoshiCoder <100981673+SatoshiCoder@users.noreply.github.com>
2022-10-10 19:52:27 +0700

    Create jaikey.py

---
+++ jaikey.py
@@ -0,0 +1,23 @@
+from colorama import Fore
+
+print(Fore.LIGHTRED_EX+'''
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                        𝓙𝓪𝓲𝓴𝓮𝔂 𝓣𝓻𝓲𝓬𝓴𝓮𝓻
+                𝑮𝒎𝒂𝒊𝒍:𝒉𝒂𝒊𝒅𝒖𝒐𝒏𝒈𝒚𝒆𝒖𝒍𝒂𝒎𝒏𝒉𝒂10@𝒈𝒎𝒂𝒊𝒍.com
+                                <3    
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+                                          ''')
+j = print(Fore.LIGHTMAGENTA_EX+'Ddos Layer 7')                                          
+a = input('[+]URL:')
+b = input('[+]THREADS:')                                          
+c = input('[+]TIME(s):')      
+atol = 'Attack status =>  '
+
+#spam = atol + a
+
+for i in range(100000):
+    print(Fore.MAGENTA + atol + a)
+
+print('Attack done!')
+
+
